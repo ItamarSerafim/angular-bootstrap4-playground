1b6703568278a2b4c937e36e6e5453932033deec
ItamarSerafim <saladatododia@hotmail.com>
2019-05-21 12:21:23 -0300

    Created app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+  VERSION
+} from "@angular/core";
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
 
+import { AngularFireDatabase } from "@angular/fire/database";
+
+type BreakPoint = { text: string; val: boolean };
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  name = "Angular";
+  ngVersion = VERSION.full;
+  windowWidth: number;
+  windowWidthGt768 = false;
+
+  private breakpointObserverSubscription;
+
+  breakPoints: BreakPoint[] = [];
+
+  // Side navigation
+  sideNavOpen = true;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    // private photoService: PhotoService
+  ) {}
+
+  ngAfterViewInit() {
+    // this.sideNav.close(null);
+    this.breakpointObserverSubscription = this.breakpointObserver
+      .observe([
+        "(min-width: 768px) and (max-width: 1024px)" /* Sidenav: open - Menu: icon */,
+        "(min-width: 980px)" /* Furture: Sidenav: open - Menu: icon+text */,
+        "(min-width: 1025px)" /* Furture: Sidenav: open, side - Menu: icon+text */
+      ])
+      .subscribe((state: BreakpointState) => {
+        this.breakPoints = Object.keys(state.breakpoints)
+          .map(str => ({ text: str, val: state.breakpoints[str] }))
+          .filter(function(item: BreakPoint) {
+            return item.val;
+          });
+      });
+  }
+
+  ngOnInit() {}
+
+  toggleSideNav() {
+  }
+
+  ngOnDestroy() {
+    this.breakpointObserverSubscription &&
+      this.breakpointObserverSubscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -20,8 +23,10 @@ import { AppComponent } from './app.component';
 @NgModule({
   imports:      [ 
     CommonModule,
-    BrowserModule, 
+    BrowserModule,  
+    LayoutModule,
     FormsModule,
+    NgbModule,
     FontAwesomeModule,
     AppRoutingModule 
   ],
